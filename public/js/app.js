cf9ed4ae0542eca2d0dc711ea361bcb1b1283ff0
Michelle Salazar <michellesalazar010@gmail.com>
2020-06-28 23:25:07 -0700

    cleanup

--- public/js/app.js
+++ public/js/app.js
@@ -3,11 +3,28 @@
 let server = 'http://localhost:3000';
 
 function loadPage(){
-$.ajax(`${server}/data/location.json`)
-.then( data =>{
-console.log(data);
+    $.ajax(`${server}/data/location.json`)
+        .then( location =>{
+    $.ajax(`${server}/data/weather.json`)
+        .then(weather =>{
+            render(map);
+            render(weather);
+        })
 });
     console.log(location);
 }
+
+function renderMap(){
+    let template = $('map-template').html();
+    let html = Mustache.render(template);
+    $('#map').append(html);
+}
+
+
+function render(weather){
+
+
+}
+
 
 loadPage();
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const cors = require('cors');
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 
 const app = express(); 
