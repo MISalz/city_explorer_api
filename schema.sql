8fa47c8bd90fd2963ee47175e1ec033882f3b077
Michelle Salazar <michellesalazar010@gmail.com>
2020-07-25 23:28:37 -0700

    connect weather api and trails api with help of Davee

---
+++ schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS locations;
+CREATE TABLE locations (
+  id SERIAL PRIMARY KEY,
+  formatted_query VARCHAR(255),
+  latitude NUMERIC,
+  longitude NUMERIC,
+  search_query VARCHAR(255) 
+);
